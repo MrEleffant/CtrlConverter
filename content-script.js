048c7382a666ab014e4e03b45ea4a214ef9ea873
MrEleffant <thibautlelouet@googlemail.com>
2022-01-16 12:30:52 +0100

    V2.0
    format et désélection

--- content-script.js
+++ content-script.js
@@ -9,15 +9,16 @@ const db = {
 document.onmouseup = function(e) {
     const content = window.getSelection().toString();
     if (db[content]) {
-        // navigator.clipboard.writeText(db[content]) // past the db correspondance in the clipboard **https only**
+        // navigator.clipboard.writeText(db[content]); // past the db correspondance in the clipboard **https only**
 
-        // window.prompt(db[content], db[content]) // open conversion window ctrl c enter
+        // window.prompt(db[content], db[content]); // open conversion window ctrl c enter
     }
 }
 
 // second way - acces to db from ctrl c
 document.addEventListener('copy', function(e) {
-    if(!db[window.getSelection().toString()]) return
-    e.clipboardData.setData('text/plain', db[window.getSelection().toString()]);
-    e.preventDefault();
+    const selection = window.getSelection().toString().trimEnd();
+    if(!db[selection]) return;
+    window.getSelection().empty();
+    e.clipboardData.setData('text/plain', db[selection]);
 });
