8127617a34b6974c5b6dc6df45846545d9d09dc8
MrEleffant <thibautlelouet@googlemail.com>
2022-01-22 12:09:00 +0100

    Update content-script.js
    auto tipe module
    ² turn on /off
    & select
    < or space to change or word

--- content-script.js
+++ content-script.js
@@ -2,8 +2,7 @@ const db = {
     "word1": "1",
     "word2": "2",
     "word3": "3"
-}
-
+   }
 
 // first way
 document.onmouseup = function(e) {
@@ -16,9 +15,56 @@ document.onmouseup = function(e) {
 }
 
 // second way - acces to db from ctrl c
-document.addEventListener('copy', function(e) {
+document.addEventListener("copy", function(e) {
     const selection = window.getSelection().toString().trimEnd();
     if(!db[selection]) return;
     window.getSelection().empty();
-    e.clipboardData.setData('text/plain', db[selection]);
+    e.clipboardData.setData("text/plain", db[selection]);
+    e.preventDefault();
 });
+
+
+let selected = null, current = null, i = 0, j =0, auto = false
+document.addEventListener("keypress", function(e) {
+    if(e.keyCode == 178) {
+        auto = !auto
+    }
+    if(!auto) return
+    switch (e.keyCode) {
+        case 38: // key "&" for selection
+            const selection = window.getSelection().toString().trimEnd();
+            if(!db[selection]) return;
+            window.getSelection().empty();
+            i=0
+            j=0
+            selected = db[selection].split(" ")
+            current = selected[i].split("")
+            break;
+
+        case 60, 32: // key ">" for change or space 
+            if(selected == null || current == null ) break
+            if(i == (selected.lenght-1) && j == (current.lenght - 1)) {
+                selected = null 
+                current = null
+                break
+            } else {
+                current = selected[++i]?.split("") || null
+                j=0
+            }
+            break;
+    
+        default:
+            e.preventDefault();
+            if(selected == null || current == null || current[j] == undefined) break
+            if(i == (selected.lenght-1) && j == (current.lenght - 1)) {
+                selected = null 
+                current = null
+                break
+            } else {
+                document.execCommand("insertText", false, current[j]);
+                j++
+            }
+            break;
+    }
+    return;
+})
